a5d31622f65634b8264a801b8cce2d860bebd0fd
Kaan Demir <146492433+wkaandemir@users.noreply.github.com>
2024-02-11 12:25:13 +0300

    update

--- get_rsi_signal.py
+++ get_rsi_signal.py
@@ -1,9 +1,10 @@
 import os  # Module for system functions
-import time  # For time-related operations
-import requests  # For sending HTTP requests
+import time  # Module for time-related operations
+import requests  # Module for sending HTTP requests
 from ta.momentum import RSIIndicator  # Using the RSIIndicator class from the TA library to calculate RSI
-import pandas as pd  # For data analysis
+import pandas as pd  # Module for data analysis
 import symbolsList  # Custom module containing symbols for trading
+from smtplib import SMTP
 
 # Base URL for the Binance API
 binance_api_url = "https://api.binance.com/api/v3"
@@ -11,44 +12,40 @@ binance_api_url = "https://api.binance.com/api/v3"
 symbols = symbolsList.symbols
 # Lower threshold for RSI
 rsi_lower_threshold = 30
-# Time interval for candlestick data (e.g., 1m, 3m, 5m, 15m, 30m, 1h, 2h, 4h)
-time_interval = "1h"
+# Time intervals for candlestick data (e.g., 1m, 3m, 5m, 15m, 30m, 1h, 2h, 4h)
+time_intervals = ["4h", "2h", "1h", "30m", "15m", "5m"]  # Updated list of time intervals
 
-# Call the API to get candlestick data for a specific symbol
+# Call API to get candlestick data for a specific symbol
 def get_candlestick_data(symbol, interval, limit):
     url = f"{binance_api_url}/klines?symbol={symbol}&interval={interval}&limit={limit}"
     response = requests.get(url)
     data = response.json()
     return data
 
-# Show a sound alert when the RSI drops below 30
+# Sound an alert when RSI drops below 30
 def sound_alert_lower():
     print("RSI dropped below 30! Alerting...\n \n ")
     os.system('afplay /System/Library/Sounds/Ping.aiff')  # Play system sound on macOS
-    time.sleep(3)
 
-# Check RSI for a specific symbol and alert if necessary
-def check_rsi(symbol):
-    candlestick_data = get_candlestick_data(symbol, time_interval, 100)  # Get candlestick data for a specific symbol
+# Check RSI for a specific symbol and show alert if necessary
+def check_rsi(symbol, interval):
+    candlestick_data = get_candlestick_data(symbol, interval, 100)  # Get candlestick data for a specific symbol
     closes = [float(entry[4]) for entry in candlestick_data]  # Get closing prices
     closes_series = pd.Series(closes)  # Create a Pandas Series
-    rsi_indicator = RSIIndicator(close=closes_series)  # Create RSI indicator
+    rsi_indicator = RSIIndicator(close=closes_series)  # Create an RSI indicator
     rsi = rsi_indicator.rsi().iloc[-1]  # Calculate RSI value
-    print(f"{symbol:<15} RSI: {rsi:.2f}")  # Print RSI value
+    print(f"{symbol:<15} RSI ({interval}): {rsi:.2f}")  # Print RSI value
     if rsi < rsi_lower_threshold:  # Check if RSI is below the threshold
-        sound_alert_lower()  # Show sound alert
-        time.sleep(3)  # Short pause between alerts
+        sound_alert_lower()  # Sound alert
+        time.sleep(1)  # Short pause between alerts
 
-# Main loop: Periodically check RSI for all symbols
+# Main loop: Regularly check RSI for all symbols
 def main():
-    while True:
+    for interval in time_intervals:  # Loop over the list of time intervals
         for symbol in symbols:
-            check_rsi(symbol)
-        for remaining_time in range(10, 0, -1):
-            print(f"Remaining time: {remaining_time} seconds")
-            time.sleep(1)
-        print("Countdown completed!")
+            check_rsi(symbol, interval)
+            #time.sleep(1)  # Short pause between each symbol
 
-# Execute the code
+# Run the code
 if __name__ == "__main__":
     main()
